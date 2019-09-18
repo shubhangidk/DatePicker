36c412271db5acb430bd93d65a767c35c42ad12a
AndroidProjects <payalvarade@gmail.com>
2019-09-18 16:37:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.datepicker;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.edittext);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog=new DatePickerDialog(MainActivity.this);
+
+            }
+        });
+
+    }
+}
